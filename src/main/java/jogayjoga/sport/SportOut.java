14198a941f3fa1c579c379951014a981ce45375f
eriksoaress <erikbtu2020@gmail.com>
2024-05-25 16:46:06 -0300

    correcoes

--- src/main/java/jogayjoga/sport/SportOut.java
+++ src/main/java/jogayjoga/sport/SportOut.java
@@ -1,11 +1,13 @@
 package jogayjoga.sport;
 import lombok.Builder;
+import java.io.Serializable;
 
 @Builder
 public record SportOut(
     String id,
     String name
-)
+) implements Serializable
 {
+    private static final long serialVersionUID = 1L;
     
 }
